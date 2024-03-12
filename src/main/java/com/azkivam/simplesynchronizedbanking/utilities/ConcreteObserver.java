d240a9390cbda83bd2b342ea8ef46141c8c022d1
mohsen <seaoceania4@gmail.com>
2024-03-12 03:47:21 +0330

    Logging edited and is now ready

--- src/main/java/com/azkivam/simplesynchronizedbanking/utilities/ConcreteObserver.java
+++ src/main/java/com/azkivam/simplesynchronizedbanking/utilities/ConcreteObserver.java
@@ -7,6 +7,8 @@ import org.springframework.stereotype.Component;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -24,11 +26,19 @@ public class ConcreteObserver implements TransactionObserver{
     @Override
     public void onTransaction(String accountNumber, String transactionType, String amount) throws IOException {
 //        print to file
-        FileWriter fileWriter = new FileWriter("M:\\master\\azkivam\\logger.txt",true);
+        FileWriter fileWriter = new FileWriter("M:\\master\\azkivam\\logfile.txt",true);
         PrintWriter printWriter = new PrintWriter(fileWriter);
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime now = LocalDateTime.now();
         printWriter.print(accountNumber);
+        printWriter.print("|");
         printWriter.print(transactionType);
+        printWriter.print("|");
         printWriter.printf(amount);
+        printWriter.print("|");
+        printWriter.print(dtf.format(now));
+        printWriter.print("|");
+        printWriter.print("\n");
         printWriter.close();
     }
 
